b589fbb025a58c638d96d5ed62e6d735532ab6fd
Fountain <fountainaigbona@gmail.com>
2025-01-30 20:37:44 +0100

    integrated db into codebase

--- index.js
+++ index.js
@@ -8,8 +8,18 @@ const port = 3000;
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+db.connect();
+
+async function getItems() {
+    // Gets all items from  the database
+    const result = await db.query("SELECT * FROM books INNER JOIN notes ON books.id = notes.book_id;");
+    console.log(result.rows);
+    return result.rows;
+  };
+
 // Handles logic for landing page
 app.get("/", async (req, res) => {
+    getItems();
     res.render("index.ejs");
 });
 
